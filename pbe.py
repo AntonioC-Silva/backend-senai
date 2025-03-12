f4ff819efa3d5dc12b418b406f71d0d121610943
AntonioC-Silva <acrscloud@gmail.com>
2025-03-12 16:50:37 -0300

    py_pbe_finalizando

---
+++ pbe.py
@@ -0,0 +1,66 @@
+#ATVD 1
+'''
+nome = "João"
+print()
+'''
+
+'''
+#ATVD 2
+a =  5
+b = 10
+soma = a+b
+subtracao = a-b
+multiplicacao = a*b
+divisao = a/b
+print(f"soma: {soma}, subtração: {subtracao}, Multiplicação: {multiplicacao}: Divisão: {divisao}.")
+'''
+
+'''
+#ATVD 3
+PRECO = 50
+desconto = 10
+predes = PRECO-desconto
+print(f" valor com o desconto é de {predes}")
+'''
+
+'''
+#ATVD4
+resultado = 10+5*2
+print(f"o Resultado é {resultado}")
+'''
+
+'''
+#ATVD5
+texto = "150"
+texto = int(texto)
+mult = texto*2
+print(mult)
+'''
+
+'''
+#ATVD 7
+a = int(input("Digite um numero: "))
+b = int(input("digite outro numero: "))
+print(a+b)
+'''
+
+'''
+#ATVD 8
+x = float(input("escreva um numero: "))
+y = float(input("escreva outro numero: "))
+div = x//y
+print(div)
+'''
+
+'''
+#ATVD 9
+num1 = float(input("Digite um numero: "))
+num2 = float(input("digite outro numero: "))
+veri = num1>num2
+print(veri)
+'''
+
+#ATVD 10
+idade = int(input("Digite sua idade: "))
+quant = idade*365
+print(f"está vivo a aproximadamente {quant} dias")
