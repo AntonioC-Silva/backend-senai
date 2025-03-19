b34309c6f70ff6cb018d09fa49b00b1484f597e8
AntonioC-Silva <acrscloud@gmail.com>
2025-03-19 16:26:59 -0300

    Atividade_python_finalizada

--- pbe.py
+++ pbe.py
@@ -60,7 +60,63 @@ veri = num1>num2
 print(veri)
 '''
 
+'''
 #ATVD 10
 idade = int(input("Digite sua idade: "))
 quant = idade*365
 print(f"está vivo a aproximadamente {quant} dias")
+'''
+
+'''
+#ATVD11
+base = float(input("Digite a base: "))
+expoente = float(input("Digite o expoente: "))
+calc = base**expoente
+print(f"O resultado é: {calc}")
+'''
+
+'''
+#ATVD12
+preco = float(input("Digite o preço: "))
+preco = str(preco)
+print(f"O preço é R$ {preco}")
+'''
+
+'''
+#ATVD13
+raio = float(input("Digite o raio: "))
+area = 3,14*(raio**2)
+print(f"A área do circulo é: {area}")
+'''
+
+'''
+#ATVD14
+a = float(input("Digite um numero: "))
+b = float(input("digite outro numero: "))
+(a, b) = (b, a)
+print(f" O valor de a agora é {a} e o valor de b agora é {b}")
+'''
+
+'''
+#ATVD 15
+nota1 = float(input("Escreva a primeira nota: "))
+nota2 = float(input("Escreva a segunda nota: "))
+nota3 = float(input("Escreva a terceira nota: "))
+peso1 = 2
+peso2 = 3
+peso3 = 5
+calc = (nota1*peso1 + nota2*peso2 + nota3*peso3)/(peso1+peso2+peso3)
+print("O resultado é:", calc)
+'''
+
+'''
+#ATVD16  DESAFIO
+x1 = float(input("Escreva a primeira coordenada: "))
+y1 = float(input("Escreva a segunda coordenada: "))
+x2 = float(input("Escreva a terceira coordenada: "))
+y2 = float(input("Escreva a terceira coordenada: "))
+distancia1 = x2-x1
+distancia2 = y2-y1
+calc = ((distancia1)**2+(distancia2)**2)**(1/2)
+print(f"A ditancia entre as coordenadas é {calc}")
+'''
